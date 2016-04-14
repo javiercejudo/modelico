bed68e74000ea928e1bd452a13fa48cad82651bc
Javier Cejudo <javier@javiercejudo.com>
2016-04-14 23:48:56 +1000

    feat: support IE9 and 10 (#19)

--- scripts/browser_tester.sh
+++ scripts/browser_tester.sh
@@ -3,9 +3,11 @@
 set -e
 
 BROWSERS=PhantomJS
+LEGACY_IE_BROWSERS=PhantomJS
 
 if [[ "$TRAVIS_NODE_VERSION" = "stable" && "$SAUCE_ACCESS_KEY" ]]; then
   BROWSERS=$SL_BROWSERS
+  LEGACY_IE_BROWSERS=$SL_LEGACY_IE_BROWSERS
 fi
 
 echo -e '\nO_o Browser tests'
@@ -14,4 +16,5 @@ echo -e '\nO_o Preparing...'
 npm run build
 
 echo -e '\nO_o Running browser tests...'
-karma start --single-run --browsers $BROWSERS
+ENTRY="index.js" karma start --single-run --browsers $BROWSERS
+ENTRY="ie9_10.js" karma start --single-run --browsers $LEGACY_IE_BROWSERS
--- test/modelicoSpec.js
+++ test/modelicoSpec.js
@@ -1,15 +1,22 @@
 'use strict';
 
-module.exports = (should, M) => _ => {
+const buildUtils = (options) => Object.freeze({
+  skipIfLegacyIE: options.legacyIE ? it.skip : it,
+  objToArr: obj => Object.keys(obj).map(k => [k, obj[k]])
+});
+
+module.exports = (options, should, M) => _ => {
+  const U = buildUtils(options);
   const deps = [should, M];
+  const utilsAndDeps = [U].concat(deps);
 
   describe('ModelicoBase', require('./types/Modelico').apply(_, deps));
-  describe('ModelicoAsIs', require('./types/AsIs').apply(_, deps));
+  describe('ModelicoAsIs', require('./types/AsIs').apply(_, utilsAndDeps));
   describe('ModelicoMap', require('./types/Map').apply(_, deps));
   describe('ModelicoEnumMap', require('./types/EnumMap').apply(_, deps));
   describe('ModelicoList', require('./types/List').apply(_, deps));
   describe('Readme Simple Example', require('./example/simple').apply(_, deps));
   describe('Readme Advanced Example', require('./example/advanced').apply(_, deps));
   describe('Readme Advanced Example ES5', require('./example/advanced.es5').apply(_, deps));
-  describe('Immutable.js examples', require('./Immutable.js/').apply(_, deps));
+  describe('Immutable.js examples', require('./Immutable.js/').apply(_, utilsAndDeps));
 };
