d20ecd735c50bae24ef73d5e255e4e4633014253
Javier Cejudo <javier@javiercejudo.com>
2016-04-30 01:55:53 +1000

    test: run suite with minified modelico

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@
 
 const Should = require('should');
 const Modelico = require('../dist/modelico.js');
+const ModelicoMin = require('../dist/modelico.min.js');
 const modelicoSpec = require('../dist/modelico-spec.js');
 
-describe('Modelico (Node.js setup)', modelicoSpec({}, Should, Modelico));
+describe('Modelico Dev (Node.js setup)', modelicoSpec({}, Should, Modelico));
+describe('Modelico Min (Node.js setup)', modelicoSpec({}, Should, ModelicoMin));
