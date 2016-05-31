0f0e645b4208921bc0d889ce2c9d165c5ac069ce
Javier Cejudo <javier@javiercejudo.com>
2016-05-31 23:23:05 +1000

    chore: es2015 build

--- src/U.js
+++ src/U.js
@@ -10,3 +10,4 @@ export const always = x => () => x;
 export const defaultTo = fallback => optional => (optional === undefined) ? fallback : optional;
 export const objToArr = obj => Object.keys(obj).map(k => [k, obj[k]]);
 export const reviverOrAsIs = pipe2(get('reviver'), defaultTo(asIsReviver));
+export const isPlainObject = x => typeof x === 'object' && !!x;
--- test/rollup.config.js
+++ test/rollup.config.js
@@ -1,10 +1,9 @@
-import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 
 export default {
   entry: './test/modelicoSpec.js',
   format: 'umd',
   moduleName: 'modelicoSpec',
-  plugins: [ json(), babel() ],
+  plugins: [ babel() ],
   dest: './dist/modelico-spec.js'
 };
