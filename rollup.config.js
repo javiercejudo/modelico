653c1f446724b9504f7093a289ce7b5a78f7d135
Javier Cejudo <javier@javiercejudo.com>
2016-08-11 22:59:07 +1000

    chore: set up Flow (#113)

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import flow from 'rollup-plugin-flow';
 
 export default {
   entry: './index.js',
   format: 'umd',
   moduleName: 'Modelico',
-  plugins: [ nodeResolve(), json(), babel() ],
+  plugins: [ flow(), nodeResolve(), json(), babel() ],
   dest: './dist/modelico.js'
 };
--- rollup.es2015.config.js
+++ rollup.es2015.config.js
@@ -1,10 +1,11 @@
 import json from 'rollup-plugin-json';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import flow from 'rollup-plugin-flow';
 
 export default {
   entry: './index.js',
   format: 'es',
   moduleName: 'Modelico',
-  plugins: [ nodeResolve(), json() ],
+  plugins: [ flow(), nodeResolve(), json() ],
   dest: './dist/modelico.es2015.js'
 };
