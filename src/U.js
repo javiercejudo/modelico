f85aae4d01f326e1f50f5110598fd0f23ef1b503
Javier Cejudo <javier@javiercejudo.com>
2016-08-11 21:36:25 +1000

    chore: set up Flow

--- src/U.js
+++ src/U.js
@@ -1,13 +1,15 @@
+// @flow
+
 'use strict';
 
-const get = field => obj => obj[field];
-const pipe2 = (fn1, fn2) => (...args) => fn2(fn1(...args));
+const get = (field: string) => (obj: Object) => obj[field];
+const pipe2 = (fn1: Function, fn2: Function) => (...args: Array<mixed>) => fn2(fn1(...args));
 
-export const pipe = (...fns) => fns.reduce(pipe2);
-export const partial = (fn, ...args) => fn.bind(undefined, ...args);
-export const asIsReviver = (k, v) => v;
-export const always = x => () => x;
-export const defaultTo = fallback => optional => (optional === undefined) ? fallback : optional;
-export const objToArr = obj => Object.keys(obj).map(k => [k, obj[k]]);
+export const pipe = (...fns: Array<Function>) => fns.reduce(pipe2);
+export const partial = (fn: Function, ...args: Array<mixed>) => fn.bind(undefined, ...args);
+export const asIsReviver = (k: string, v: mixed) => v;
+export const always = (x: mixed) => () => x;
+export const defaultTo = (fallback: mixed) => (optional: mixed) => (optional === undefined) ? fallback : optional;
+export const objToArr = (obj: Object) => Object.keys(obj).map(k => [k, obj[k]]);
 export const reviverOrAsIs = pipe2(get('reviver'), defaultTo(asIsReviver));
-export const isPlainObject = x => typeof x === 'object' && !!x;
+export const isPlainObject = (x: mixed) => typeof x === 'object' && !!x;
