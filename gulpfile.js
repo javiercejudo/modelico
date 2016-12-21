545ef7210f3ec14133fa897a389608f4ea5bb540
Javier Cejudo <javier@javiercejudo.com>
2016-12-22 02:40:47 +1100

    style: gulpfile and karma conf to standard

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
-'use strict';
+'use strict'
 
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
-const istanbul = require('gulp-istanbul');
-const rimraf = require('rimraf');
-const codecov = require('gulp-codecov');
-const plumber = require('gulp-plumber');
+const gulp = require('gulp')
+const mocha = require('gulp-mocha')
+const istanbul = require('gulp-istanbul')
+const rimraf = require('rimraf')
+const codecov = require('gulp-codecov')
+const plumber = require('gulp-plumber')
 
-let coverageVariable;
+let coverageVariable
 
-gulp.task('clean', cb => rimraf('./coverage', cb));
+gulp.task('clean', cb => rimraf('./coverage', cb))
 
 gulp.task('instrument', ['clean'], () => {
-  coverageVariable = '$$cov_' + new Date().getTime() + '$$';
+  coverageVariable = '$$cov_' + new Date().getTime() + '$$'
 
   return gulp.src([
-      'dist/modelico.js'
-    ])
+    'dist/modelico.js'
+  ])
     .pipe(plumber())
     .pipe(istanbul({ coverageVariable }))
-    .pipe(istanbul.hookRequire());
-});
+    .pipe(istanbul.hookRequire())
+})
 
 gulp.task('test', ['clean', 'instrument'], () => {
   return gulp.src(['test/index.js'])
     .pipe(mocha({ bail: false }))
-    .pipe(istanbul.writeReports({ coverageVariable }));
-});
+    .pipe(istanbul.writeReports({ coverageVariable }))
+})
 
-gulp.task('codecov', () => gulp.src('coverage/coverage-final.json').pipe(codecov()));
+gulp.task('codecov', () => gulp.src('coverage/coverage-final.json').pipe(codecov()))
 
-gulp.task('default', ['test']);
+gulp.task('default', ['test'])
--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,14 @@
 // Karma configuration
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -21,39 +19,31 @@ module.exports = function(config) {
       'test/browser/' + (process.env.ENTRY || 'index.js')
     ],
 
-
     // list of files to exclude
     exclude: [],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
     },
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['dots', 'saucelabs'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     sauceLabs: {
       username: process.env.SAUCE_USERNAME,
@@ -64,31 +54,26 @@ module.exports = function(config) {
       testName: 'modelico: browser tests'
     },
 
-
     customLaunchers: customLaunchers(),
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    //browsers: ['Chrome', 'Firefox'],
+    // browsers: ['Chrome', 'Firefox'],
     browsers: Object.keys(customLaunchers()),
-
 
     // timeout for capturing a browser (in ms)
     captureTimeout: 0,
 
-
     // how long will Karma wait for a message from a browser before disconnecting from it (in ms).
     browserNoActivityTimeout: 0,
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  });
-};
+  })
+}
 
-function customLaunchers() {
+function customLaunchers () {
   return {
     'SL_Chrome': {
       base: 'SauceLabs',
@@ -148,5 +133,5 @@ function customLaunchers() {
       platform: 'OS X 10.10',
       version: '9.2'
     }
-  };
+  }
 }
